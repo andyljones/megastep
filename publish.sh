4fe191c5c2f357a2c118d285ee8cc3c4d57a7154
Andrew Jones <andyjones.ed@gmail.com>
2020-07-17 12:32:47 +0000

    Adds nojekyll file

--- publish.sh
+++ publish.sh
@@ -3,8 +3,9 @@
 # Copied from https://stackoverflow.com/questions/48588908/deploying-ignored-dist-folder-to-github-pages
 
 sphinx-build -E -b html docs docs/_build
+touch docs/_build/.nojekyll
 git branch --delete --force gh-pages
 git checkout --orphan gh-pages
 git add -f docs/_build
 git commit -m "Rebuild GitHub pages"
-git filter-branch -f --prune-empty --subdirectory-filter docs/_build && git push -f origin gh-pages && git checkout -
+git filter-branch -f --prune-empty --subdirectory-filter docs/_build && git push -f origin gh-pages && git checkout master
